a081ccab521a9ab6e0987bc04151a368a976eba7
byfranfx <71705230+byfranfx@users.noreply.github.com>
2020-10-07 19:55:13 +0200

    Change the text of the button and form

--- MyFirstSPDVIproject_WinformsUI/Form1.Designer.cs
+++ MyFirstSPDVIproject_WinformsUI/Form1.Designer.cs
@@ -33,7 +33,8 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.ForeColor = System.Drawing.Color.Coral;
+            this.button1.Location = new System.Drawing.Point(391, 166);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
