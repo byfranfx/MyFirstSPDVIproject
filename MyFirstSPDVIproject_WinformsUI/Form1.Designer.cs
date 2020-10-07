98bfd7d4c88b51058775f880a9c4b585cc82c417
byfranfx <71705230+byfranfx@users.noreply.github.com>
2020-10-07 20:19:02 +0200

    added Readme.md file

--- MyFirstSPDVIproject_WinformsUI/Form1.Designer.cs
+++ MyFirstSPDVIproject_WinformsUI/Form1.Designer.cs
@@ -33,13 +33,14 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.ForeColor = System.Drawing.Color.Coral;
             this.button1.Location = new System.Drawing.Point(391, 166);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
