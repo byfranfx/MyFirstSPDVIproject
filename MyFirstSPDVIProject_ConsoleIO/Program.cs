dddb69460ef899775bdf28b91eaacebb48894f33
byfranfx <71705230+byfranfx@users.noreply.github.com>
2020-10-07 19:36:45 +0200

    first commit

---
+++ MyFirstSPDVIProject_ConsoleIO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstSPDVIProject_ConsoleIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SPDVI 2020 - 2021");
+            Console.ReadKey();
+
+        }
+    }
+}
